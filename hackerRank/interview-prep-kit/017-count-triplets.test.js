a6a78b8c965da4ae707ef1c36a80b1511699d1ed
Victoria Cheng <victoria.cheng15@gmail.com>
2022-06-05 13:04:48 -0400

    uncomment last 2 test cases

--- hackerRank/interview-prep-kit/017-count-triplets.test.js
+++ hackerRank/interview-prep-kit/017-count-triplets.test.js
@@ -17,7 +17,7 @@ function countTriplets(arr, r) {
 describe('count triplets', () => {
   it('should return the amount of triplets', () => {
     expect(countTriplets([1, 2, 2, 4], 2)).toBe(2);
-    // expect(countTriplets([1, 3, 9, 9, 27, 81], 3)).toBe(6);
-    // expect(countTriplets([1, 5, 5, 25, 125], 5)).toBe(4);
+    expect(countTriplets([1, 3, 9, 9, 27, 81], 3)).toBe(6);
+    expect(countTriplets([1, 5, 5, 25, 125], 5)).toBe(4);
   });
 });
